6466e8c5ed88712180a837477f8bac4bb7bae88d
Oussama <earsvision1@gmail.com>
2025-11-07 14:28:25 +0100

    IMplement api endpoints

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
+  
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
